a4c557afd02bf9101a4b91fd8553be374f066946
Rudy Bermudez <hello@rudybermudez.co>
2016-05-31 17:48:55 +0200

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Rudy Bermudez on 5/31/16.
+//  Copyright © 2016 Rudy Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
