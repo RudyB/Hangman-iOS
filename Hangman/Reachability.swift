026911a1fc916410373c6a63ad0e6086baeb33b9
Rudy Bermudez <hello@rudybermudez.io>
2016-09-22 16:52:53 -0700

    Updates Reachability to Swift 3

--- Hangman/Reachability.swift
+++ Hangman/Reachability.swift
@@ -11,20 +11,26 @@ import SystemConfiguration
 
 open class Reachability {
 	class func isConnectedToNetwork() -> Bool {
-//		var zeroAddress = sockaddr_in()
-//		zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
-//		zeroAddress.sin_family = sa_family_t(AF_INET)
-//		let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
-//			SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
-//		}
-//		var flags = SCNetworkReachabilityFlags()
-//		if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
-//			return false
-//		}
-//		let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
-//		let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
-//		return (isReachable && !needsConnection)
-		return true
+		var zeroAddress = sockaddr()
+		zeroAddress.sa_len = UInt8(MemoryLayout<sockaddr>.size)
+		zeroAddress.sa_family = sa_family_t(AF_INET)
+		
+		guard let defaultRouteReachability: SCNetworkReachability = withUnsafePointer(to: &zeroAddress, {
+			SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+		}) else { return false }
+		
+		
+		var flags : SCNetworkReachabilityFlags = []
+		if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+			return false
+		}
+		
+		
+		let isReachable = flags.contains(.reachable)
+		let needsConnection = flags.contains(.connectionRequired)
+		
+		
+		return (isReachable && !needsConnection)
 	}
 	enum ConnectivityErrors: Error {
 		case noActiveInternetConnection
